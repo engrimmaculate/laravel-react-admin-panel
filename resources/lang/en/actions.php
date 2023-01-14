20e6cbc3e55106cb73569d9ab65772b79a70ee7e
Eagles Digital <104556862+engrimmaculate@users.noreply.github.com>
2023-01-14 11:11:32 +0100

    Initialize dependabot/npm_and_yarn/urijs-1.19.11

---
+++ resources/lang/en/actions.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Action Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in common actions such as
+    | button texts, links, message titles & content etc.
+    |
+    */
+
+    'back' => 'Back',
+    'next' => 'Next',
+    'confirm' => 'Confirm',
+    'cancel' => 'Cancel',
+    'continue' => 'Continue',
+    'retry' => 'Retry',
+    'undo' => 'Undo',
+];
---
+++ resources/lang/en/resources.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Resource Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in the frontend of the application,
+    | specifically in resource links, actions etc.
+    |
+    */
+
+    'create' => 'Add :name',
+    'show' => 'View :name',
+    'edit' => 'Edit :name',
+    'delete' => 'Delete :name',
+    'edit_image' => "Edit :name's image",
+
+    'delete_confirmation_title' => 'You are deleting a :name',
+    'delete_confirmation_body' => "If not undone, the :name won't be recovered anymore.",
+
+    'fetched' => 'Fetched :total :names.',
+    'not_fetched' => 'Error fetching :names!',
+    'created' => ':name sucessfully created!',
+    'not_created' => 'Error creating :name!',
+    'updated' => ':name sucessfully updated!',
+    'not_updated' => 'Error updating :name!',
+    'deleted' => ':name successfully deleted!',
+    'not_deleted' => 'Error deleting :name!',
+    'restored' => ':name successfully restored!',
+    'not_restored' => 'Error restoring :name!',
+];
